0767708430baf406cd037a41c694e03d9266dd5b
David-Lee-dev <aganga7427@gmail.com>
2026-01-06 13:33:40 +0900

    [Fix] remove global shortcut & add file menu as shortcut

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,9 +2,8 @@ mod domains;
 mod infrastructure;
 mod utils;
 
-use tauri_plugin_global_shortcut::GlobalShortcutExt;
+use crate::utils::shortcuts::AppShortcuts;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-use utils::shortcuts::{global_shortcut_handler, AppShortcuts};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -15,19 +14,11 @@ pub fn run() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let shortcuts = AppShortcuts::new();
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(
-            tauri_plugin_global_shortcut::Builder::new()
-                .with_handler(global_shortcut_handler)
-                .build(),
-        )
-        .setup(move |app| {
-            for shortcut in shortcuts.all() {
-                app.global_shortcut().register(shortcut)?;
-            }
+        .setup(|app| {
+            AppShortcuts::setup_menu(app.handle())?;
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
@@ -35,6 +26,7 @@ pub fn run() {
             domains::config::command::save_config,
             domains::config::command::init_database,
             domains::config::command::load_database,
+            // Note
             domains::note::command::select_storage,
         ])
         .run(tauri::generate_context!())
--- src-tauri/src/utils/shortcuts.rs
+++ src-tauri/src/utils/shortcuts.rs
@@ -1,31 +1,32 @@
-use tauri::{AppHandle, Emitter};
-use tauri_plugin_global_shortcut::{Code, Modifiers, Shortcut, ShortcutEvent, ShortcutState};
-pub struct AppShortcuts {
-    pub close_tab: Shortcut,
-    pub new_note: Shortcut,
-}
+// utils/shortcuts.rs
+use tauri::{
+    menu::{Menu, MenuItem, Submenu},
+    AppHandle, Emitter,
+};
+
+pub struct AppShortcuts;
 
 impl AppShortcuts {
-    pub fn new() -> Self {
-        Self {
-            close_tab: Shortcut::new(Some(Modifiers::SUPER), Code::KeyW),
-            new_note: Shortcut::new(Some(Modifiers::SUPER), Code::KeyN),
-        }
-    }
+    pub fn setup_menu(app: &AppHandle) -> tauri::Result<()> {
+        let app_menu = Submenu::with_id(app, "app", "App", true)?;
+        let file_menu = Submenu::with_id(app, "file", "File", true)?;
 
-    pub fn all(&self) -> Vec<Shortcut> {
-        vec![self.close_tab, self.new_note]
-    }
-}
+        let close_tab =
+            MenuItem::with_id(app, "close-tab", "Close Tab", true, Some("CmdOrCtrl+W"))?;
+        let new_note = MenuItem::with_id(app, "new-note", "New Note", true, Some("CmdOrCtrl+N"))?;
 
-pub fn global_shortcut_handler(app: &AppHandle, shortcut: &Shortcut, event: ShortcutEvent) {
-    let shortcuts = AppShortcuts::new();
+        file_menu.append(&close_tab)?;
+        file_menu.append(&new_note)?;
 
-    if event.state() == ShortcutState::Pressed {
-        if shortcut == &shortcuts.close_tab {
-            let _ = app.emit("shortcut-event", "close-tab");
-        } else if shortcut == &shortcuts.new_note {
-            let _ = app.emit("shortcut-event", "new-note");
-        }
+        let menu = Menu::with_items(app, &[&app_menu, &file_menu])?;
+
+        app.set_menu(menu)?;
+
+        let handle = app.clone();
+        app.on_menu_event(move |_, event| {
+            let _ = handle.emit("shortcut-event", event.id().as_ref());
+        });
+
+        Ok(())
     }
 }
